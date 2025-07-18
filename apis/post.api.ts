86a7511d2c2d3e16c6964e1a779b4b0235cbd0a3
Mohammad m <mashhadim901@gmail.com>
2025-07-18 18:21:14 +0430

    feature(feature/singlePost):add single post page

--- apis/post.api.ts
+++ apis/post.api.ts
@@ -2,3 +2,8 @@ export const fetchData = async () => {
   const url = "https://dummyjson.com/posts";
   return await fetch(url).then((r) => r.json());
 };
+
+export const fetchPost = async (postId: string) => {
+  const url = `https://dummyjson.com/posts/${postId}`;
+  return await fetch(url).then((r) => r.json());
+};
