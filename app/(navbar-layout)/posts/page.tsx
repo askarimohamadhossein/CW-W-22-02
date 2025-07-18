01ec47ccc0b501c9f08256c3b362e714b0655093
Fateme Zahra Abdinasab <fatemehzahraabdinasab@gmail.com>
2025-07-18 16:51:07 +0330

    feat(feature/post): Create post card and render post list (#1)

--- app/(navbar-layout)/posts/page.tsx
+++ app/(navbar-layout)/posts/page.tsx
@@ -1,9 +1,14 @@
 import { fetchData } from "@/apis/post.api";
+import PostCard from "@/components/molecules/PostCard";
 
 export default async function PostPage() {
+  const data: IPostList = await fetchData();
   return (
     <div className="p-5">
       <p className="font-semibold text-lg">Post List</p>
+      {data.posts.map((el) => (
+        <PostCard post={el} key={el.id} />
+      ))}
     </div>
   );
 }
